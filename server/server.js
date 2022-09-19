6a19f7f4a598359c90c46239f5548d8e0f169cad
Theo Selin <theo.selin@hotmail.com>
2022-09-19 10:58:56 +0200

    Mongoose install & UserSchema

--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
-const express = require("express")
-const morgan = require("morgan")
-const bodyParser = require("body-parser")
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
 // Middleware
-app.use(morgan("dev"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Get data from server
 app.get("/", (req, res) => {
-    res.json("Hello")
-})
+  res.json("Hello");
+});
 
 // Send data to backend
 app.post("/", (req, res) => {
-    console.log(req.body.name)
-})
+  console.log(req.body.name);
+});
 
-app.listen(3000, err => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log("listening on PORT", 3000)
-    }
-})
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("listening on PORT", 3000);
+  }
+});
