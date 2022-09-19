bcc6584e535aaf96612b279961424f2f79c5db4e
Theo Selin <theo.selin@hotmail.com>
2022-09-19 16:35:11 +0200

    API routes

--- server/server.js
+++ server/server.js
@@ -23,26 +23,9 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Get data from server
-app.get("/", (req, res) => {
-  res.json("Hello");
-});
-
-// Send data to backend
-app.post("/", (req, res) => {
-  let user = new User();
-  user.name = req.body.name;
-  user.email = req.body.email;
-  user.password = req.body.password;
-
-  user.save((err) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json("Successfully saved user");
-    }
-  });
-});
+// require APIs
+const productRoutes = require("./routes/product");
+app.use("/api", productRoutes);
 
 app.listen(3000, (err) => {
   if (err) {
