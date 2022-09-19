c4dfdbebc65d7a0c95e5e3add09f9bee2c6b6d4d
Theo Selin <theo.selin@hotmail.com>
2022-09-19 13:38:02 +0200

    Product, owner, category schemas

--- server/server.js
+++ server/server.js
@@ -4,20 +4,19 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const { default: mongoose } = require("mongoose");
 
-dotenv.config()
+const User = require("./models/user");
+
+dotenv.config();
 
 const app = express();
 
-mongoose.connect(
-  process.env.DATABASE,
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Connected to the database");
-    }
+mongoose.connect(process.env.DATABASE, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
   }
-);
+});
 
 // Middleware
 app.use(morgan("dev"));
@@ -31,7 +30,18 @@ app.get("/", (req, res) => {
 
 // Send data to backend
 app.post("/", (req, res) => {
-  console.log(req.body.name);
+  let user = new User();
+  user.name = req.body.name;
+  user.email = req.body.email;
+  user.password = req.body.password;
+
+  user.save((err) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json("Successfully saved user");
+    }
+  });
 });
 
 app.listen(3000, (err) => {
