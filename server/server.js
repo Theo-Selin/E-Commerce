b98fed6d7d006e6971a5ba18d340d579c30bd882
Theo Selin <theo.selin@hotmail.com>
2022-09-19 11:26:48 +0200

    Database connection & .env security

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+
+dotenv.config()
 
 const app = express();
+
+mongoose.connect(
+  process.env.DATABASE,
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to the database");
+    }
+  }
+);
 
 // Middleware
 app.use(morgan("dev"));
