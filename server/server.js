900bb6edf7ceeb4d3027db79acaafec4f2653dec
Theo Selin <theo.selin@hotmail.com>
2022-09-27 23:49:27 +0200

    Category POST & GET routes

--- server/server.js
+++ server/server.js
@@ -25,7 +25,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // require APIs
 const productRoutes = require("./routes/product");
+const categoryRoutes = require("./routes/category");
 app.use("/api", productRoutes);
+app.use("/api", categoryRoutes);
 
 app.listen(3000, (err) => {
   if (err) {
