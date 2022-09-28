490b6ff8e858664aee868ff4dcd08566f0d3fba6
Theo Selin <theo.selin@hotmail.com>
2022-09-28 20:29:46 +0200

    UPDATE, DELETE, GETONE, GET ALL - products

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Product = require("../models/product");
 
-const upload = require("../middlewares/upload-photo")
+const upload = require("../middlewares/upload-photo");
 
 // POST request - create a new product
 router.post("/products", upload.single("photo"), async (req, res) => {
@@ -27,11 +27,84 @@ router.post("/products", upload.single("photo"), async (req, res) => {
 });
 
 // GET request - get all products
+router.get("/products", async (req, res) => {
+  try {
+    let products = await Product.find();
+    res.json({
+      success: true,
+      products: products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 // GET request - get a single product
+router.get("/products/:id", async (req, res) => {
+  try {
+    let product = await Product.findOne({ _id: req.params.id });
+    res.json({
+      success: true,
+      product: product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 // PUT request - Update a single product
+router.put("/products/:id", upload.single("photo"), async (req, res) => {
+  try {
+    let product = await Product.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          title: req.body.title,
+          price: req.body.price,
+          category: req.body.categoryID,
+          photo: req.file.location,
+          description: req.body.description,
+          owner: req.body.ownerID,
+        },
+      },
+      { upsert: true }
+    );
+
+    res.json({
+      success: true,
+      updatedProduct: product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 // DELETE request - deletes a single product
+router.delete("/products/:id", async (req, res) => {
+  try {
+    let deletedProduct = await Product.findOneAndDelete({ _id: req.params.id })
+
+    if (deletedProduct) {
+      res.json({
+        status: true,
+        message: "Successfully deleted product"
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 module.exports = router;
