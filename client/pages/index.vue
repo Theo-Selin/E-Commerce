fff6d5fab44b61f4d6df8c25bf603c699bc3ecb3
Theo Selin <theo.selin@hotmail.com>
2022-09-29 19:26:04 +0200

    Navbar setup - html

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <nuxt-link to="/products" class="a-button-buy-again"
+      >Add a new product</nuxt-link
+    >
+  </div>
 </template>
 
 <script>
+import Navbar from "~/components/Navbar";
 export default {
-  name: 'IndexPage'
-}
+  components: {
+    Navbar,
+  },
+};
 </script>
