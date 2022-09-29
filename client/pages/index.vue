fab8ac9dcf5d1bccd0c07c1f41c530e3957ade01
Theo Selin <theo.selin@hotmail.com>
2022-09-29 19:42:47 +0200

    Footer template installation

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="container">
+  <div>
+    <Navbar />
     <nuxt-link to="/products" class="a-button-buy-again"
       >Add a new product</nuxt-link
     >
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from "~/components/Navbar";
+import Footer from "~/components/Footer.vue";
 export default {
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
