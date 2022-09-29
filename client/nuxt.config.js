8fbb2338bc8d97308313c5607306edd5b929ed9f
Theo Selin <theo.selin@hotmail.com>
2022-09-29 23:36:32 +0200

    Render all products & populate owner/category

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,3 +1,5 @@
+const URL = "http://localhost:8000";
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -45,7 +47,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "/",
+    proxy: true,
+    baseURL: URL,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
