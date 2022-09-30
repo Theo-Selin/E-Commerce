31486c84c82f986920a2bb28bb09e4162e36920c
Theo Selin <theo.selin@hotmail.com>
2022-09-30 14:14:46 +0200

    manual default.vue file

--- client/.nuxt/layouts/default.vue
+++ client/.nuxt/layouts/default.vue
@@ -1,18 +1,3 @@
 <template>
-  <Nuxt-Layout>
-    <Navbar />
-    <Nuxt />
-    <Footer />
-  </Nuxt-Layout>
+  <Nuxt />
 </template>
-
-<script>
-import Navbar from "~/components/Navbar";
-import Footer from "~/components/Footer";
-export default {
-  components: {
-    Navbar,
-    Footer,
-  },
-};
-</script>
