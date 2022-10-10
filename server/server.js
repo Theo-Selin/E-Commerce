3c6a70379314e1de29163f4855fe39492c74994f
Theo Selin <theo.selin@hotmail.com>
2022-10-10 18:54:24 +0200

    Order, payment and search

--- server/server.js
+++ server/server.js
@@ -33,6 +33,8 @@ const userRoutes = require("./routes/auth");
 const reviewRoutes = require("./routes/review");
 const addressRoutes = require("./routes/address");
 const paymentRoutes = require("./routes/payment");
+const orderRoutes = require("./routes/order");
+const searchRoutes = require("./routes/search");
 
 app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
@@ -41,6 +43,8 @@ app.use("/api", userRoutes);
 app.use("/api", reviewRoutes);
 app.use("/api", addressRoutes);
 app.use("/api", paymentRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", searchRoutes);
 
 app.listen(8000, (err) => {
   if (err) {
